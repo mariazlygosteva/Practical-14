a6621a2d1f9fcfb2b6bf6d85b0cdb29959e386f2
Fedotova Maria <159981092+mariazlygosteva@users.noreply.github.com>
2025-11-26 11:29:51 +0700

    task_5.py

--- task_5.py
+++ task_5.py
@@ -1,3 +1,16 @@
-numbers = list(map(int, input().split()))
-average = sum(numbers) / len(numbers)
-print(average)
+def calculate_average() -> float:
+    """
+    Calculates arithmetic mean of input numbers.
+
+    Args:
+        None: Reads space-separated integers from standard input.
+
+    Returns:
+        float: Arithmetic mean of the input numbers.
+    """
+    numbers = list(map(int, input().split()))
+    average = sum(numbers) / len(numbers)
+    return average
+
+
+print(calculate_average())
