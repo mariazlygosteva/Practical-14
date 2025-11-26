a2e9bbfd42da670be289ffc710562d207de832b5
Fedotova Maria <159981092+mariazlygosteva@users.noreply.github.com>
2025-11-26 11:31:07 +0700

    task_7.py

--- task_7.py
+++ task_7.py
@@ -1,4 +1,18 @@
-numbers = list(map(int, input().split()))
-even_sum = sum(x for x in numbers if x % 2 == 0)
-odd_sum = sum(x for x in numbers if x % 2 != 0)
-print(even_sum, odd_sum)
+def calculate_sums() -> tuple[int, int]:
+    """
+    Calculates sum of even and odd numbers from input list.
+
+    Args:
+        None: Reads space-separated integers from standard input.
+
+    Returns:
+        tuple[int, int]: Tuple containing (sum_of_evens, sum_of_odds).
+    """
+    numbers = list(map(int, input().split()))
+    even_sum = sum(x for x in numbers if x % 2 == 0)
+    odd_sum = sum(x for x in numbers if x % 2 != 0)
+    return even_sum, odd_sum
+
+
+even, odd = calculate_sums()
+print(even, odd)
