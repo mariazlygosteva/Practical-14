f33e7088a649feab8ad14255cfc19c91681356b2
Fedotova Maria <159981092+mariazlygosteva@users.noreply.github.com>
2025-11-26 11:33:48 +0700

    task_9.py

--- task_9.py
+++ task_9.py
@@ -1,41 +1,53 @@
 import string
 
 
-text_lines = []
+def process_text() -> None:
+    """
+    Processes multi-line text to count word frequency and output in descending order.
 
-while True:
-    line = input()
+    Args:
+        None: Reads multi-line text from standard input until empty line.
 
-    if line == '':
-        break
-    text_lines.append(line)
+    Returns:
+        None: Prints words in descending order of frequency.
+    """
+    text_lines = []
 
-full_text = ' '.join(text_lines).lower()
-translator = str.maketrans('', '', string.punctuation)
-clean_text = full_text.translate(translator)
-words = clean_text.split()
+    while True:
+        line = input()
+        if line == '':
+            break
+        text_lines.append(line)
 
-word_count = {}
-order = []
+    full_text = ' '.join(text_lines).lower()
+    translator = str.maketrans('', '', string.punctuation)
+    clean_text = full_text.translate(translator)
+    words = clean_text.split()
 
-for word in words:
-    if word not in word_count:
-        word_count[word] = 0
-        order.append(word)
-    word_count[word] += 1
+    word_count = {}
+    order = []
 
-word_freq = []
-a = word_freq
+    for word in words:
+        if word not in word_count:
+            word_count[word] = 0
+            order.append(word)
+        word_count[word] += 1
 
-for word in order:
-    word_freq.append((word_count[word], word))
+    word_freq = []
+    a = word_freq
 
-for i in range(len(a)):
-    for j in range(i + 1, len(a)):
-        if a[i][0] < a[j][0]:
-            a[i], a[j] = a[j], a[i]
+    for word in order:
+        word_freq.append((word_count[word], word))
 
-sorted_words = [pair[1] for pair in a]
+    for i in range(len(a)):
+        for j in range(i + 1, len(a)):
+            if a[i][0] < a[j][0]:
+                a[i], a[j] = a[j], a[i]
 
-for word in sorted_words:
-    print(word)
+    sorted_words = [pair[1] for pair in a]
+
+    for word in sorted_words:
+        print(word)
+
+
+process_text()
