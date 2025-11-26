43b6a1ee642ef9d57b32611b3102c41191c269be
Fedotova Maria <159981092+mariazlygosteva@users.noreply.github.com>
2025-11-26 11:30:28 +0700

    task_6.py

--- task_6.py
+++ task_6.py
@@ -1,3 +1,16 @@
-num = int(input())
-divisors = [i for i in range(1, num + 1) if num % i == 0]
-print(divisors)
+def find_divisors() -> list[int]:
+    """
+    Returns list of all divisors of input number in ascending order.
+
+    Args:
+        None: Reads an integer from standard input.
+
+    Returns:
+        list[int]: All divisors of the input number in ascending order.
+    """
+    num = int(input())
+    divisors = [i for i in range(1, num + 1) if num % i == 0]
+    return divisors
+
+
+print(find_divisors())
