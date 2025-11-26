5dce5d7cf49acba017fc45b7c19134df8b5f6c16
Fedotova Maria <159981092+mariazlygosteva@users.noreply.github.com>
2025-11-26 11:28:11 +0700

    task_2.py

--- task_2.py
+++ task_2.py
@@ -1,3 +1,16 @@
-numbers = list(map(int, input().split()))
-numbers.remove(3)
-print(numbers)
+def process_numbers() -> list[int]:
+    """
+    Removes first occurrence of number 3 from input list.
+
+    Args:
+        None: Reads space-separated integers from standard input.
+
+    Returns:
+        list[int]: Modified list with first occurrence of 3 removed.
+    """
+    numbers = list(map(int, input().split()))
+    numbers.remove(3)
+    return numbers
+
+
+print(process_numbers())
