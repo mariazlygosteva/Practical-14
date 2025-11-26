8e79f0b3a2ae3d0fa0408983688b9e16cd807926
Fedotova Maria <159981092+mariazlygosteva@users.noreply.github.com>
2025-11-26 11:34:48 +0700

    task_10.py

--- task_10.py
+++ task_10.py
@@ -1,9 +1,23 @@
-lst1 = list(map(int, input().split()))
-lst2 = list(map(int, input().split()))
-start, end = map(int, input().split())
+def move_elements() -> None:
+    """
+    Moves reversed slice from first list to second list and prints both modified lists.
 
-elements_to_move = lst1[start - 1:end][::-1]
-lst2.extend(elements_to_move)
-lst1 = lst1[:start - 1] + lst1[end:]
+    Args:
+        None: Reads all inputs from standard input.
 
-print(lst1, lst2, sep='\n')
+    Returns:
+        None: Results are printed to standard output.
+    """
+    lst1 = list(map(int, input().split()))
+    lst2 = list(map(int, input().split()))
+    start, end = map(int, input().split())
+
+    elements_to_move = lst1[start - 1:end][::-1]
+    lst2.extend(elements_to_move)
+    lst1 = lst1[:start - 1] + lst1[end:]
+
+    print(lst1)
+    print(lst2)
+
+
+move_elements()
